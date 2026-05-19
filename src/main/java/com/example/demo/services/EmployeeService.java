5c60fa4a3b8a31b3ccf113ae57a37e4db1dca098
Richa <richa2907@gmail.com>
2026-05-19 15:26:57 +0530

    building GET by id api

--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -4,4 +4,5 @@ import com.example.demo.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeByid(Long employeeid);
 }
