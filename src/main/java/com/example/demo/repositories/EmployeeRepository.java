b2e1c6fc6b0e5e4bd96fa96a1a0ea46d02d33d5a
Rich2907 <richa.shaw2907@gmail.com>
2026-05-19 22:01:24 +0530

    againn_for the last

--- src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repositories;
 import com.example.demo.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//using JPA repostory we can perform crud operations on our table
+//using JPA repository we can perform crud operations on our table
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
