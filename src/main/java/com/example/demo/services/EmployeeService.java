73326ab38fcc0176cccdb6371dc8559c8000ced7
Richa <richa2907@gmail.com>
2026-05-19 15:48:09 +0530

    building GET all api

--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -2,7 +2,10 @@ package com.example.demo.services;
 
 import com.example.demo.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
     EmployeeDto getEmployeeByid(Long employeeid);
+    List<EmployeeDto> getAllEmployee();
 }
